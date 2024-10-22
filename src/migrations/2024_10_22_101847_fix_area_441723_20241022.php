d0ae3cf7e374f899f6df808014db1f40e8644b6a
Xhiny <axhiny@163.com>
2024-10-22 18:22:32 +0800

    将阳东县改为阳东区

---
+++ src/migrations/2024_10_22_101847_fix_area_441723_20241022.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class FixArea44172320241022 extends Migration
+{
+
+    public function beforeCmmUp()
+    {
+        //
+    }
+
+    public function beforeCmmDown()
+    {
+        //
+    }
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        DB::table('qs_area')->where('id', 441723)->update(['cname'=>'阳东区']);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        DB::table('qs_area')->where('id', 441723)->update(['cname'=>'阳东县']);
+    }
+
+    public function afterCmmUp()
+    {
+        //
+    }
+
+    public function afterCmmDown()
+    {
+        //
+    }
+}
