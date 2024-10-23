98dde39c3588483139b7d7342d352e420a662076
Xhiny <axhiny@163.com>
2024-10-23 15:39:51 +0800

    将阳东县改为阳东区

--- src/ChineseRegionProvider.php
+++ src/ChineseRegionProvider.php
@@ -2,13 +2,18 @@
 namespace Qscmf\ChineseRegion;
 
 use Bootstrap\LaravelProvider;
+use Bootstrap\Provider;
 use Bootstrap\RegisterContainer;
 
-class ChineseRegionProvider implements LaravelProvider{
+class ChineseRegionProvider implements LaravelProvider, Provider {
 
     public function registerLara(): void
     {
         RegisterContainer::registerMigration(__DIR__.'/migrations');
     }
 
+    public function register()
+    {
+
+    }
 }
